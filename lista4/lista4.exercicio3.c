0b69be420998800fd3c5c1f99a3eb45dc205cb8c
pedrokpp <pedrokp@id.uff.br>
2024-04-10 14:37:48 -0300

    lista 4 ex 3

---
+++ lista4/lista4.exercicio3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+	int numbers[5];
+	int *p;
+	int n;
+
+	p = numbers;
+	*p = 10;
+	p++;
+	*p = 20;
+	p = &numbers[2];
+	*p = 30;
+	p = numbers + 3;
+	*p = 40;
+	p = numbers;
+	*(p + 4) = 50;
+
+	for (n = 0; n < 5; n++) {
+		printf("%d\n", numbers[n]);
+	}
+}
